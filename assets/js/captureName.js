70b9044aa185c9ca1ccacddcd66fa35a7de19543
Lucas Filgueiras <lucks_fil@hotmail.com>
2022-06-05 20:17:04 -0300

    novas interfaces

--- assets/js/captureName.js
+++ assets/js/captureName.js
@@ -1,11 +1,20 @@
 function captureName() {
     const professorButton = document.querySelector(".professor");
+    const alunoButton = document.querySelector(".aluno");
 
     professorButton.addEventListener("click", () => {
         const name = document.querySelector(".name");
         const nameValue = name.value;
         sessionStorage.setItem("Nome", nameValue);
     })
+
+    alunoButton.addEventListener("click", () => {
+        const name = document.querySelector(".name");
+        const nameValue = name.value;
+        sessionStorage.setItem("Nome", nameValue);
+    })
+
+
 }
 
 captureName();
